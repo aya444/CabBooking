d5e4c1597615d61749df306b2a1b0380a80365f5
NFayed <norhan.fayed1412@gmail.com>
2021-12-29 15:57:40 +0200

    ride manager,ride persistance
    edited ride manager
    and added ride persistance

---
+++ src/main/java/com/example/demo/Presistence/RideStoreByArray.java
@@ -0,0 +1,14 @@
+package com.example.demo.Presistence;
+
+//import java.util.ArrayList;
+
+import com.example.demo.Presistence.StorageManagement;
+import com.example.demo.Core.User;
+
+public class StoreByArray implements StorageManagement{
+	public void SaveAs(User user){
+		//ArrayList<User> arraystore=new ArrayList<>();
+	}
+
+	
+}
